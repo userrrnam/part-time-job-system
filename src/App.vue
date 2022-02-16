97aac114f3050a65e2a549b4bed4d049750c1184
wanghao <wanghao@yunqilaohe.com>
2022-02-16 16:14:51 +0800

    feat: update

--- src/App.vue
+++ src/App.vue
@@ -19,31 +19,5 @@ body {
 #app {
   width: 100vw;
   background: rgba(245, 245, 245);
-  .slide-fade-enter-active {
-    transition: all 0.3s ease-out;
-  }
-
-  .slide-fade-leave-active {
-    transition: all 0.3s ease-out;
-  }
-
-  .slide-fade-enter-from,
-  .slide-fade-leave-to {
-    transform: translateX(30px);
-    opacity: 0;
-  }
-  .down-up-enter-active {
-    transition: all 0.3s ease-out;
-  }
-
-  .down-up-leave-active {
-    transition: all 0.3s ease-out;
-  }
-
-  .down-up-enter-from,
-  .down-up-leave-to {
-    transform: translateY(-44px);
-    opacity: 0;
-  }
 }
 </style>
